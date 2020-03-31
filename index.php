ae7d53f8bf105ea63e728ff67784d6a279b2a209
RvanHulst <99051006@mydavinci.nl>
2020-03-31 11:05:37 +0200

    index , main.css and navbar added

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php 
+
+	include "navbar.php";
+ ?>
+ <!DOCTYPE html>
+ <html>
+ <head>
+ 	<title>ToDoList</title>
+ 	<link rel="stylesheet" type="text/css" href="main.css">
+ </head>
+ <body>
+ 	<p>Hello world</p>
+ 
+ </body>
+ </html>
