db3b875437021f44239c3e98bb7b56cb1f619296
Raaf <99051006@mydavinci.nl>
2020-04-15 10:45:33 +0200

    overview update

--- overview.php
+++ overview.php
@@ -8,11 +8,38 @@
  require "include/head.php";
  ?>
  <body>
- <p class="w3-xlarge">Welcome on Index</p>
-	 <p>Bye World</p>
- <?php
+ <body>
+    <div class="container">
+        <h1>To-Do List</h1>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">Lijst</th>
+                    <th scope="col"></th>
+                </tr>
+            </thead>
+            <tbody>
+                    <tr>
+                        <td><?php echo $row['list_name'] ?></td>
+                        <td class="text-right">
+                            <a class="btn btn-success" href=' '>
+                            </a>
+                            <a class="btn btn-warning" href=''>
+                            </a>
+                            <a class="btn btn-danger" href=''>
+                            </a>
+                        </td>
+                    </tr>
+                <tr>
+                </tr>
+            </tbody>
+        </table>
+        <div class="col-12 text-center">
+            <a class="btn btn-primary text-white" href="php/createList.php">Nieuwe lijst aanmaken</a>
+        </div>
+    </div>
+    <?php
     require "include/footer.php";
-?>
- </body>
- </html>
-  
+    ?>
+</body>
+</html>
